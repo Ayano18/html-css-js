4ef7522f421d7635cde606aee10224a8117fca8a
Ayano18 <timothyalvarez51@gmail.com>
2026-02-10 22:13:13 +0800

    commit

---
+++ GastadorWebsite/gastador.js
@@ -0,0 +1,92 @@
+
+const tim = document.querySelector(".tim");
+const marc = document.querySelector(".marc");
+const chin = document.querySelector(".chin");
+const genel = document.querySelector(".genel");
+const shane = document.querySelector(".shane");
+const jastine = document.querySelector(".jastine");
+const kate = document.querySelector(".kate");
+const alyssa = document.querySelector(".alyssa");
+const kurt = document.querySelector(".kurt");
+
+
+function linwels(){
+    const linwel = document.querySelector(".linwel");
+    if (linwel.style.display === "none" || linwel.style.display === "") {
+        linwel.style.display = "block";
+    } else {
+        linwel.style.display = "none";
+    }
+}
+
+function tims(){
+    if (tim.style.display === "none" || tim.style.display === "") {
+        tim.style.display = "block";
+    } else {
+        tim.style.display = "none";
+    }
+}
+
+function toggleMarc(){
+    if (marc.style.display === "none" || marc.style.display === "") {
+        marc.style.display = "block";
+    } else {
+        marc.style.display = "none";
+    }
+}
+
+function toggleChin(){
+    if (chin.style.display === "none" || chin.style.display === "") {
+        chin.style.display = "block";
+    } else {
+        chin.style.display = "none";
+    }
+}
+
+function toggleGenel(){
+    if (genel.style.display === "none" || genel.style.display === "") {
+        genel.style.display = "block";
+    } else {
+        genel.style.display = "none";
+    }
+}
+
+function toggleShane(){
+    if (shane.style.display === "none" || shane.style.display === "") {
+        shane.style.display = "block";
+    } else {
+        shane.style.display = "none";
+    }
+}
+
+function toggleJastine(){
+    if (jastine.style.display === "none" || jastine.style.display === "") {
+        jastine.style.display = "block";
+    } else {
+        jastine.style.display = "none";
+    }
+}
+
+function toggleKate(){
+    if (kate.style.display === "none" || kate.style.display === "") {
+        kate.style.display = "block";
+    } else {
+        kate.style.display = "none";
+    }
+}
+
+function toggleAlyssa(){
+    if (alyssa.style.display === "none" || alyssa.style.display === "") {
+        alyssa.style.display = "block";
+    } else {
+        alyssa.style.display = "none";
+    }
+}
+
+function toggleKurt(){
+    if (kurt.style.display === "none" || kurt.style.display === "") {
+        kurt.style.display = "block";
+    } else {
+        kurt.style.display = "none";
+    }
+}
